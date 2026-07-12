ad4e1bb5b04710c6a365c1662181bffa89c3f113
vksaab999-afk <vksaab999@gmail.com>
2026-07-12 12:10:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from threading import Thread
 # Config
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 MONGO_URI = os.environ.get('MONGO_URI')
-ADMIN_ID = 5785924075 # Aapki ID set kar di hai
+ADMIN_ID = 5785924075 
 
 bot = telebot.TeleBot(BOT_TOKEN)
 client = MongoClient(MONGO_URI)
@@ -43,22 +43,39 @@ def list_users(message):
             msg += f"{user['uid']}\n"
         bot.reply_to(message, msg if len(msg) < 4000 else "List bahut badi hai.")
 
-@bot.message_handler(content_types=['photo', 'video', 'document', 'text'])
+# Broadcast Handler with Voice, Photo, Video, and HTML support
+@bot.message_handler(content_types=['photo', 'video', 'document', 'text', 'voice', 'audio'])
 def broadcast(message):
-    if message.from_user.id == ADMIN_ID and message.text not in ['/start', '/stats', '/list']:
-        all_users = list(users_col.find())
-        for user in all_users:
-            try:
-                if message.content_type == 'photo': 
-                    bot.send_photo(user['uid'], message.photo[-1].file_id, caption=message.caption)
-                elif message.content_type == 'video': 
-                    bot.send_video(user['uid'], message.video.file_id, caption=message.caption)
-                elif message.content_type == 'document': 
-                    bot.send_document(user['uid'], message.document.file_id, caption=message.caption)
-                else: 
-                    bot.send_message(user['uid'], message.text)
-            except: pass
-        bot.reply_to(message, "✅ Broadcast complete!")
+    if message.from_user.id != ADMIN_ID:
+        return
+
+    if message.text in ['/start', '/stats', '/list']:
+        return
+
+    all_users = list(users_col.find())
+    
+    # HTML text ya Caption extract karna
+    display_text = message.html_text if message.content_type == 'text' else message.caption
+    if display_text is None: display_text = ""
+
+    for user in all_users:
+        try:
+            if message.content_type == 'photo':
+                bot.send_photo(user['uid'], message.photo[-1].file_id, caption=display_text, parse_mode='HTML')
+            elif message.content_type == 'video':
+                bot.send_video(user['uid'], message.video.file_id, caption=display_text, parse_mode='HTML')
+            elif message.content_type == 'document':
+                bot.send_document(user['uid'], message.document.file_id, caption=display_text, parse_mode='HTML')
+            elif message.content_type == 'voice':
+                bot.send_voice(user['uid'], message.voice.file_id, caption=display_text, parse_mode='HTML')
+            elif message.content_type == 'audio':
+                bot.send_audio(user['uid'], message.audio.file_id, caption=display_text, parse_mode='HTML')
+            else:
+                bot.send_message(user['uid'], display_text, parse_mode='HTML')
+        except:
+            pass
+
+    bot.reply_to(message, "✅ Premium Broadcast complete!")
 
 if __name__ == '__main__':
     Thread(target=keep_alive).start()
